cd6b784ea155762676426f1dd84bc6de9cf03cea
nazarenko.sergiy <nazarenko.sergiy@gmail.com>
2019-12-31 19:20:41 +0100

    added example of pretty colors to calendar view

--- aligntime/Calendar/CalendarManager.swift
+++ aligntime/Calendar/CalendarManager.swift
@@ -17,8 +17,7 @@ struct CalendarManager: View {
        @State var deselectedIsPresented = false
        
        var rkManager1 = RKManager(calendar: Calendar.current, minimumDate: Date(), maximumDate: Date().addingTimeInterval(60*60*24*365), mode: 0)
-       var pickup_date = "Pick up the date"
-       
+   
        var body: some View {
         
            VStack (spacing: 25) {
@@ -28,17 +27,15 @@ struct CalendarManager: View {
        }
        
        func startUp() {
+         let testOnDates = [Date().addingTimeInterval(60*60*24), Date().addingTimeInterval(60*60*24*2)]
+         rkManager1.selectedDates.append(contentsOf: testOnDates)
          
-           
+         // example of some foreground colors
+         rkManager1.colors.weekdayHeaderColor = Color.blue
+         rkManager1.colors.monthHeaderColor = Color.green
+         rkManager1.colors.textColor = Color.blue
+         rkManager1.colors.disabledColor = Color.red
        }
-       
-       func getTextFromDate(date: Date!) -> String {
-           let formatter = DateFormatter()
-           formatter.locale = .current
-           formatter.dateFormat = "yyyy.MM.d"
-           return date == nil ? "Pick up the date" : formatter.string(from: date)
-       }
-
 }
 
 struct Calendar_Previews: PreviewProvider {
