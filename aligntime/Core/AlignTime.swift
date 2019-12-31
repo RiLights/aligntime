56bbd983cfb33af2128a7dc394ec4d8ffc2e22cf
Ostap <rilights@gmail.com>
2019-12-31 23:19:29 +1300

    Basic functional class

---
+++ aligntime/Core/AlignTime.swift
@@ -0,0 +1,26 @@
+//
+//  AlignTime.swift
+//  aligntime
+//
+//  Created by Ostap on 31/12/19.
+//  Copyright © 2019 Ostap. All rights reserved.
+//
+
+import Foundation
+
+class AlignTime {
+    @Published var wear_timer:String = "00:00:00"
+    @Published var out_timer:String = "00:00:00"
+
+    func start_timer(){}
+    
+    func stop_timer(){}
+    
+    func get_total_wear_time() ->String{return ""}
+    
+    func get_total_off_time() ->String{return ""}
+    
+    func get_wear_time_for_date(date:Date) ->String{return ""}
+    
+    func get_off_time_for_date(date:Date) ->String{return ""}
+}
