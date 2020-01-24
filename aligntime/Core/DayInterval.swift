e88c6caac47410210e51e006e993118830d0d96d
Ostap <rilights@gmail.com>
2020-01-24 23:04:09 +1300

    cleaned interval section

--- aligntime/Core/DayInterval.swift
+++ aligntime/Core/DayInterval.swift
@@ -9,39 +9,9 @@
 import Foundation
 
 
-
-class Day: Identifiable,ObservableObject { //IntervalRepresentation
-    var id: Int = 0
-    var start_time_string: String = "s...."
-    var end_time_string: String = "e...."
-    var min_time:Date = get_min_time()
-    var max_time:Date = Date()
-    var original_date:Int = 0
-    var state:Bool = true
-    var day:String = ""
-    @Published var current_date:Bool = false {
-        didSet {
-            self.end_time_string = "Now"
-        }
-    }
-    @Published var end_time:Date = Date() {
-       didSet {
-           self.end_time_string = clock_string_format(self.end_time)!
-       }
-   }
-
-    @Published var start_time:Date = Date() {
-        didSet {
-            self.start_time_string = clock_string_format(self.start_time)!
-        }
-    }
-}
-
 class DayInterval: Identifiable,ObservableObject {
     var id: Int = 0
-    var time_string: String = "s...."
-    var min_time:Date = get_min_time()
-    var max_time:Date = Date()
+    var time_string: String = "...."
     var wear:Bool = true
     var day:String = ""
 
