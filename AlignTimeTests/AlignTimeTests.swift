543fe4849005241a2d58a1426231755e5d3f3538
Ostap <rilights@gmail.com>
2020-02-14 00:16:01 +1300

    tests correction. sorry

--- AlignTimeTests/AlignTimeTests.swift
+++ AlignTimeTests/AlignTimeTests.swift
@@ -230,6 +230,27 @@ class AlignTimeTests: XCTestCase {
         XCTAssertEqual(test, [])
     }
     
+    func test_get_wear_days_02(){
+        let day0 = dateFormatter.date(from: "2019-07-11 10:00")!
+        let day1 = dateFormatter.date(from: "2019-07-11 20:00")!
+        let day2 = dateFormatter.date(from: "2019-07-12 10:00")!
+        let day3 = dateFormatter.date(from: "2019-07-12 20:00")!
+        
+        let d00 = DayInterval(0, wear: false, time: day0)
+        let d01 = DayInterval(1, wear: true, time: day1)
+        let d02 = DayInterval(2, wear: false, time: day2)
+        let d03 = DayInterval(3, wear: true, time: day3)
+    
+        let align_time:AlignTime = AlignTime()
+        align_time.intervals = [d00,d01,d02,d03]
+        align_time.selected_date = day3
+        
+        let test = align_time.get_wear_days()
+
+        let correct_data = [d01]
+        XCTAssertEqual(test, correct_data)
+    }
+    
     func test_get_off_days_01(){
         let day0 = dateFormatter.date(from: "2019-07-11 10:00")!
         let day1 = dateFormatter.date(from: "2019-07-11 20:00")!
@@ -286,11 +307,11 @@ class AlignTimeTests: XCTestCase {
     
         let align_time:AlignTime = AlignTime()
         align_time.intervals = [d01,d02,d03]
-        align_time.selected_date = dateFormatter.date(from: "2019-12-07 01:00")
+        align_time.selected_date = day3
         
         let test = align_time.get_off_days()
         
-        let correct_data:[DayInterval] = [d01,d03]
+        let correct_data:[DayInterval] = [d01]
         XCTAssertEqual(test, correct_data)
     }
     
@@ -307,11 +328,12 @@ class AlignTimeTests: XCTestCase {
     
         let align_time:AlignTime = AlignTime()
         align_time.intervals = [d00,d01,d02,d03]
-        align_time.selected_date = dateFormatter.date(from: "2019-07-12 01:00")
+        align_time.selected_date = day2
         
         let test = align_time.get_off_days()
 
-        XCTAssertEqual(test, [])
+        let correct_data:[DayInterval] = [d01]
+        XCTAssertEqual(test, correct_data)
     }
     
     func test_get_off_days_05(){
@@ -327,11 +349,11 @@ class AlignTimeTests: XCTestCase {
     
         let align_time:AlignTime = AlignTime()
         align_time.intervals = [d00,d01,d02,d03]
-        align_time.selected_date = dateFormatter.date(from: "2019-07-12 01:00")
+        align_time.selected_date = day2
         
         let test = align_time.get_off_days()
 
-        let correct_data = [d00,d02]
+        let correct_data = [d02]
         XCTAssertEqual(test, correct_data)
     }
     
