c6c5218b792f821a6d2924d91d4ac2be96bf3eea
Ostap <rilights@gmail.com>
2020-09-18 12:08:12 +1200

    average_time for statistic

--- aligntime/Profile/StatisticsView.swift
+++ aligntime/Profile/StatisticsView.swift
@@ -8,79 +8,9 @@
 
 import SwiftUI
 
-struct StatisticData {
-    var date: String
-    var minutes: Int
-}
-
 struct StatisticsView: View {
     @EnvironmentObject var core_data: AlignTime
     let calendar = Calendar.current
-    @State private var hideHorizontalLines: Bool = false
-    let hours:[Int] = [0,3,6,9,12,15,18,21,24]
-    
-    var date_formatter: DateFormatter {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .short
-        return formatter
-    }
-    
-    func get_dates()->[Date]{
-        let dates = self.test_date()
-        var res:[Date] = []
-        res.append(dates.first)
-        let count:Double = Double(dates.count)
-        let val = count/8
-        for i in stride(from: 1, to: count, by: val) {
-            res.append(dates[Int(i)])
-        }
-        res.append(dates.last)
-        return res
-    }
-    
-    func test_date()->[Date]{
-        var mydates:[Date] = []
-        let fmt = DateFormatter()
-        fmt.dateFormat = "yyy-MM-dd"
-        var dateFrom =  Date() // First date
-        var dateTo = Date()   // Last date
-        dateFrom = core_data.minimumDate
-        dateTo = core_data.maximumDate
-
-
-        while dateFrom <= dateTo {
-            mydates.append(dateFrom)
-            dateFrom = Calendar.current.date(byAdding: .day, value: 1, to: dateFrom)!
-
-        }
-        return mydates
-    }
-    
-    func test_minutes()->[Double]{
-        var mytime : [Double] = []
-        let fmt = DateFormatter()
-        fmt.dateFormat = "yyy-MM-dd"
-        var dateFrom =  Date() // First date
-        var dateTo = Date()   // Last date
-        dateFrom = core_data.minimumDate
-        dateTo = core_data.maximumDate
-
-        while dateFrom <= dateTo {
-            let val = self.core_data.total_wear_time_for_date(date:dateFrom)
-            mytime.append(Double(val/60))
-            dateFrom = Calendar.current.date(byAdding: .day, value: 1, to: dateFrom)!
-        }
-        return mytime
-    }
-    
-    func test_average()->Int{
-        let intArray = test_minutes()
-        let sumArray = intArray.reduce(0, +)
-
-        let avgArrayValue = sumArray/Double(intArray.count)
-        
-        return Int(avgArrayValue/60)
-    }
     
     func get_total_wear_time_for_past(day_index:Int)->CGFloat{
         let date_offset = Calendar.current.date(byAdding: .day, value: -day_index, to: Date())
@@ -93,17 +23,32 @@ struct StatisticsView: View {
         dateFormatterPrint.dateFormat = "dd"
         //dateFormatterPrint.dateStyle = .short
         
-        let date_offset = Calendar.current.date(byAdding: .day, value: -day_index, to: Date())
+        let date_offset = calendar.date(byAdding: .day, value: -day_index, to: Date())
         return dateFormatterPrint.string(from: date_offset!)
     }
     
     func get_month_for_past(day_index:Int)->String{
         let dateFormatterPrint = DateFormatter()
         dateFormatterPrint.dateFormat = "MMM"
-        //dateFormatterPrint.dateStyle = .short
         
-        let date_offset = Calendar.current.date(byAdding: .day, value: -day_index, to: Date())
+        let date_offset = calendar.date(byAdding: .day, value: -day_index, to: Date())
         return dateFormatterPrint.string(from: date_offset!)
+    }
+    
+    func get_average_time_for_range(range:Int)->String{
+        var sum = 0.0
+        for i in 1...range{
+            sum += self.core_data.total_wear_time_for_date(date:calendar.date(byAdding: .day, value: -(i), to: Date())!)
+        }
+        
+        let average = (sum/Double(range)+60)
+        if average.minutes == 0{
+            return "\(average.hours)"
+        }
+        if average.minutes<=10{
+            return "\(average.hours):\(average.minutes)0"
+        }
+        return "\(average.hours):\(average.minutes)"
     }
     
     var stat_range = ["7", "30"]
@@ -111,7 +56,7 @@ struct StatisticsView: View {
     
     var body: some View {
         VStack{
-            Picker(selection: self.$selected_stat_range, label: Text("hello")) {
+            Picker(selection: self.$selected_stat_range, label: Text("")) {
                 ForEach((0...1), id: \.self) { i in
                     Text("\(self.stat_range[i]) ")
                     + Text(NSLocalizedString("days",comment:""))
@@ -120,26 +65,37 @@ struct StatisticsView: View {
             VStack{
                 HStack{
                     Spacer()
-                    Text(NSLocalizedString("Average wear time: ",comment:""))
-                        + Text("\(test_average())")
-                            .foregroundColor(.blue)
-                        + Text(NSLocalizedString("per_hour",comment:""))
+                    if self.selected_stat_range == 0 {
+                        Text(NSLocalizedString("Average wear time: ",comment:""))
+                            + Text("\(get_average_time_for_range(range:7))")
+                                .foregroundColor(.blue)
+                            + Text(NSLocalizedString("per_hour",comment:""))
+                    }
+                    else if self.selected_stat_range == 1 {
+                        Text(NSLocalizedString("Average wear time: ",comment:""))
+                            + Text("\(get_average_time_for_range(range:30))")
+                                .foregroundColor(.blue)
+                            + Text(NSLocalizedString("per_hour",comment:""))
+                    }
                     Spacer()
                 }
                 HStack(spacing:0){
                     VStack{
                         Text(NSLocalizedString("Hours",comment:""))
-                        .padding(.vertical,4)
+                        .padding(.vertical,2)
                         Text("24")
-                        .padding(.vertical,4)
+                        Spacer()
                         Group{
                             ForEach(stride(from: 0, to: 24, by: 4).reversed(), id: \.self) { i in
                                 Text("\(i)")
-                                    .padding(.vertical,4)
+                                Spacer()
                             }
+                            Spacer()
                         }
                         Spacer()
-                    }.padding(.leading,20)
+                    }
+                    .padding(.leading,20)
+                    .padding(.bottom,12)
                     Spacer()
                     if self.selected_stat_range == 0{
                         ForEach((1...7).reversed(), id: \.self) { i in
@@ -214,3 +170,4 @@ struct StatisticsView: View {
         .navigationBarTitle(Text(NSLocalizedString("Time Statistic",comment:"")), displayMode: .large)
     }
 }
+
